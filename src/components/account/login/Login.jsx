03d1e3cff440a45730855c15b4045b493fa35b2f
Muss999 <mussangle1@gmail.com>
2023-09-04 21:22:05 +0600

    added login and register forms

--- src/components/account/login/Login.jsx
+++ src/components/account/login/Login.jsx
@@ -1,8 +1,120 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./login.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../../store/user/userActions";
+import {
+    clearStatusState,
+    clearUserState,
+} from "../../../store/user/userSlice";
+import logFoto from "./images/log-foto.png";
 
 const Login = () => {
-    return <div>Login</div>;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+    });
+
+    const { loading, status } = useSelector((state) => state.user);
+
+    useEffect(() => {
+        dispatch(clearStatusState);
+        dispatch(clearUserState);
+    }, []);
+
+    return (
+        <>
+            {loading ? (
+                <h3>Loading...</h3>
+            ) : (
+                <>
+                    {status ? (
+                        <>
+                            {status === "wrong" && (
+                                <>
+                                    <div>
+                                        <h3>Error!!!</h3>
+                                        <button
+                                            onClick={() =>
+                                                dispatch(clearStatusState())
+                                            }
+                                        >
+                                            Try Again!
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <div className={styles.login_form}>
+                                <div className={styles.log_left}>
+                                    <div className={styles.log_left_inner}>
+                                        <h2 className={styles.log_h2}>
+                                            We’ve missed you!
+                                        </h2>
+                                        <p className={styles.log_text}>
+                                            A lot of questions are waiting for
+                                            your wise suggestions!
+                                        </p>
+                                        <div className={styles.log_left_inp}>
+                                            <input
+                                                className={
+                                                    styles.log_form_inputs
+                                                }
+                                                type="text"
+                                                placeholder="Username"
+                                                onChange={(e) =>
+                                                    setUser({
+                                                        ...user,
+                                                        username:
+                                                            e.target.value,
+                                                    })
+                                                }
+                                            />
+                                            <input
+                                                className={
+                                                    styles.log_form_inputs
+                                                }
+                                                type="password"
+                                                placeholder="Password"
+                                                onChange={(e) =>
+                                                    setUser({
+                                                        ...user,
+                                                        password:
+                                                            e.target.value,
+                                                    })
+                                                }
+                                            />
+                                            <button
+                                                className={styles.log_btn}
+                                                onClick={() =>
+                                                    dispatch(
+                                                        loginUser({
+                                                            user,
+                                                            navigate,
+                                                        })
+                                                    )
+                                                }
+                                            >
+                                                Login
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.log_foto}>
+                                    <img src={logFoto} alt="eror" />
+                                </div>
+                            </div>
+                        </>
+                    )}
+                </>
+            )}
+        </>
+    );
 };
 
 export default Login;
--- src/store/user/userSlice.js
+++ src/store/user/userSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { registerUser, loginUser } from "./userActions";
+import { addDataToLocalStorage, updateToken } from "../../helpers/functions";
 
 const userSlice = createSlice({
     name: "user",
@@ -7,6 +9,46 @@ const userSlice = createSlice({
         loading: false,
         status: "",
     },
+    reducers: {
+        clearStatusState: (state) => {
+            state.status = "";
+        },
+        clearUserState: (state) => {
+            state.user = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.loading = false;
+                action.payload.navigate("/login");
+            })
+            .addCase(registerUser.rejected, (state) => {
+                state.loading = false;
+                state.status = "wrong";
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload.user;
+                addDataToLocalStorage(
+                    action.payload.user,
+                    action.payload.res.data
+                );
+                updateToken();
+                action.payload.navigate("/");
+            })
+            .addCase(loginUser.rejected, (state) => {
+                state.loading = false;
+                state.status = "error";
+            });
+    },
 });
 
+export const { clearStatusState, clearUserState } = userSlice.actions;
 export default userSlice.reducer;
