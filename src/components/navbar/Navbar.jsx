17deadd401f5ef588cf7bd52b77cd4baccbd0a42
Arzymatiq <keepa3003gmail.com>
2023-09-05 17:16:49 +0600

    musi prosty

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import styles from "./navbar.module.css";
 import { useNavigate } from "react-router-dom";
 import logo from "./img/загружено-removebg-preview.png";
 import userPluce from "./img/user-plus-1-removebg-preview.png";
+import askQuestion from "./img/262038-removebg-preview.png";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    let user = localStorage.getItem("user");
     return (
         <>
             <div className={styles.navBar}>
@@ -15,21 +17,34 @@ const Navbar = () => {
                     <img src={logo} width="40%" alt="" />
                 </div>
                 <div className={styles.right__block}>
-                    <button
-                        className={styles.register__btn}
-                        onClick={() => navigate("/register")}>
-                        <img
-                            src={userPluce}
-                            alt=""
-                            className={styles.register_img}
-                        />
-                        register
-                    </button>
-                    <button
-                        className={styles.login__btn}
-                        onClick={() => navigate("/login")}>
-                        login
-                    </button>
+                    {user ? (
+                        <button className={styles.add_question_btn}>
+                            <img
+                                src={askQuestion}
+                                alt=""
+                                className={styles.askIcon}
+                            />
+                            ask question
+                        </button>
+                    ) : (
+                        <>
+                            <button
+                                className={styles.register__btn}
+                                onClick={() => navigate("/register")}>
+                                <img
+                                    src={userPluce}
+                                    alt=""
+                                    className={styles.register_img}
+                                />
+                                register
+                            </button>
+                            <button
+                                className={styles.login__btn}
+                                onClick={() => navigate("/login")}>
+                                login
+                            </button>
+                        </>
+                    )}
                 </div>
             </div>
         </>
