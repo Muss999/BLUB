23393221c0ec3ec923b8916049f3c4c1b89a7546
Muss999 <mussangle1@gmail.com>
2023-09-06 17:24:06 +0600

    added navbar profile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router-dom";
 import logo from "./img/загружено-removebg-preview.png";
 import userPluce from "./img/user-plus-1-removebg-preview.png";
 import askQuestion from "./img/262038-removebg-preview.png";
+import notificationTrue from "./img/notification-true.png";
+import notificationFalse from "./img/notification-false.png";
+import profileFoto from "./img/profile-pic.png";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -19,14 +22,25 @@ const Navbar = () => {
                 </div>
                 <div className={styles.right__block}>
                     {user ? (
-                        <button className={styles.add_question_btn}>
-                            <img
-                                src={askQuestion}
-                                alt=""
-                                className={styles.askIcon}
-                            />
-                            Ask question
-                        </button>
+                        <>
+                            <button className={styles.add_question_btn}>
+                                <img
+                                    src={askQuestion}
+                                    alt=""
+                                    className={styles.askIcon}
+                                />
+                                Ask question
+                            </button>
+
+                            {/* // сюда надо добавить тернарку на то что есть
+                            уведомление или нет */}
+                            <div className={styles.notificationFoto}>
+                                <img src={notificationFalse} alt="" />
+                            </div>
+                            <div className={styles.profileFoto}>
+                                <img src={profileFoto} alt="profile-foto" />
+                            </div>
+                        </>
                     ) : (
                         <>
                             <button
